668be373f6edaa4918e564a942a8b7fcb60ebb51
baranyigabor <gabor.baranyi92@gmail.com>
2026-03-13 21:10:25 +0100

    set tolerance level

--- src/cello_moveit_config/launch/move_group.launch.py
+++ src/cello_moveit_config/launch/move_group.launch.py
@@ -1,7 +1,69 @@
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_move_group_launch
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.parameter_descriptions import ParameterValue
+from moveit_configs_utils.launch_utils import DeclareBooleanLaunchArg, add_debuggable_node
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("cello_description", package_name="cello_moveit_config").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+    moveit_config = MoveItConfigsBuilder(
+        "cello_description", package_name="cello_moveit_config"
+    ).to_moveit_configs()
+
+    ld = LaunchDescription()
+
+    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
+    ld.add_action(
+        DeclareBooleanLaunchArg("allow_trajectory_execution", default_value=True)
+    )
+    ld.add_action(
+        DeclareBooleanLaunchArg("publish_monitored_planning_scene", default_value=True)
+    )
+    ld.add_action(DeclareLaunchArgument("capabilities", default_value=""))
+    ld.add_action(DeclareLaunchArgument("disable_capabilities", default_value=""))
+    ld.add_action(DeclareBooleanLaunchArg("monitor_dynamics", default_value=False))
+
+    should_publish = LaunchConfiguration("publish_monitored_planning_scene")
+
+    move_group_configuration = {
+        "publish_robot_description_semantic": True,
+        "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
+        "capabilities": ParameterValue(
+            LaunchConfiguration("capabilities"), value_type=str
+        ),
+        "disable_capabilities": ParameterValue(
+            LaunchConfiguration("disable_capabilities"), value_type=str
+        ),
+        "publish_planning_scene": should_publish,
+        "publish_geometry_updates": should_publish,
+        "publish_state_updates": should_publish,
+        "publish_transforms_updates": should_publish,
+        "monitor_dynamics": False,
+    }
+
+    trajectory_execution = {
+        "moveit_manage_controllers": False,
+        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
+        "trajectory_execution.allowed_goal_duration_margin": 0.5,
+        "trajectory_execution.allowed_start_tolerance": 0.05,
+    }
+
+    move_group_params = [
+        moveit_config.to_dict(),
+        move_group_configuration,
+        trajectory_execution,
+    ]
+
+    add_debuggable_node(
+        ld,
+        package="moveit_ros_move_group",
+        executable="move_group",
+        commands_file=str(moveit_config.package_path / "launch" / "gdb_settings.gdb"),
+        output="screen",
+        parameters=move_group_params,
+        extra_debug_args=["--debug"],
+    )
+
+    return ld
